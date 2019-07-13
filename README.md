5b146acece49aeaacf3d2fc6fe2f0b3d1173ac65
brandonxiang <1542453460@qq.com>
2019-07-13 11:10:50 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # svelte-webpack-template
+
+To create a new project based on this template using degit:
+
+```shell
+npx degit brandonxiang/svelte-webpack-template svelte-app
+cd svelte-app
+yarn
+```
+
+## development
+
+```shell
+npm run dev
+```
---
+++ webpack.config.js
@@ -0,0 +1,66 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const path = require('path');
+
+const mode = process.env.NODE_ENV || 'development';
+const prod = mode === 'production';
+
+module.exports = {
+	entry: {
+		bundle: ['./src/main.js']
+	},
+	resolve: {
+		extensions: ['.mjs', '.js', '.svelte']
+	},
+	output: {
+		path: __dirname + '/build',
+		filename: '[name].js',
+		chunkFilename: '[name].[id].js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.svelte$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'svelte-loader',
+					options: {
+						emitCss: true,
+						hotReload: true
+					}
+				}
+			},
+			{
+				test: /\.css$/,
+				use: [
+					/**
+					 * MiniCssExtractPlugin doesn't support HMR.
+					 * For developing, use 'style-loader' instead.
+					 * */
+					prod ? MiniCssExtractPlugin.loader : 'style-loader',
+					'css-loader'
+				]
+			}
+		]
+	},
+	mode,
+	plugins: [
+		new MiniCssExtractPlugin({
+			filename: '[name].css'
+		}),
+		new HtmlWebpackPlugin({
+			template: './public/index.html',
+		}),
+		new CopyPlugin([
+			{ from: './public', to: './static' },
+		]),
+	],
+	devtool: prod ? false: 'source-map',
+	devServer: {
+		contentBase: path.join(__dirname, 'build'),
+		compress: true,
+		port: 9000,
+		open: true
+	}
+};
