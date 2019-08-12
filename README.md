b14f5f6670749ec5353da01823b3be6ceb71349c
Weiping Xiang <weiping.xiang@shopee.com>
2019-08-12 17:47:49 +0800

    feat: change project into mpa

--- README.md
+++ README.md
@@ -1,17 +1,21 @@
-# svelte-webpack-template
+# svelte-webpack-mpa
 
 <a href="https://github.com/brandonxiang/svelte-webpack-template/blob/master/LICENSE">
   <img src="https://img.shields.io/github/license/brandonxiang/svelte-webpack-template" alt="license">
 </a>
+
+The template is built for multi-page application(MPA). If you want to create a new project with SPA, see [template-webpack](https://github.com/sveltejs/template-webpack) or [webpack_svelte](https://github.com/theartkod/webpack_svelte)
 
 ## Usage
 
 To create a new project based on this template using degit:
 
 ```shell
-npx degit brandonxiang/svelte-webpack-template svelte-app
+npx degit brandonxiang/svelte-webpack-mpa svelte-app
 cd svelte-app
 yarn
+# or
+npm i
 ```
 
 ## Development
--- webpack.config.js
+++ webpack.config.js
@@ -2,65 +2,95 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 const path = require('path');
+const globby = require('globby');
 
 const mode = process.env.NODE_ENV || 'development';
 const prod = mode === 'production';
 
-module.exports = {
-	entry: {
-		bundle: ['./src/main.js']
-	},
-	resolve: {
-		extensions: ['.mjs', '.js', '.svelte']
-	},
-	output: {
-		path: __dirname + '/dist',
-		filename: '[name].js',
-		chunkFilename: '[name].[id].js'
-	},
-	module: {
-		rules: [
-			{
-				test: /\.svelte$/,
-				exclude: /node_modules/,
-				use: {
-					loader: 'svelte-loader',
-					options: {
-						emitCss: true,
-						hotReload: true
-					}
-				}
-			},
-			{
-				test: /\.css$/,
-				use: [
-					/**
-					 * MiniCssExtractPlugin doesn't support HMR.
-					 * For developing, use 'style-loader' instead.
-					 * */
-					prod ? MiniCssExtractPlugin.loader : 'style-loader',
-					'css-loader'
-				]
-			}
-		]
-	},
-	mode,
-	plugins: [
-		new MiniCssExtractPlugin({
-			filename: '[name].css'
-		}),
-		new HtmlWebpackPlugin({
-			template: './public/index.html',
-		}),
-		new CopyPlugin([
-			{ from: './public', to: './static' },
-		]),
-	],
-	devtool: prod ? false: 'source-map',
-	devServer: {
-		contentBase: path.join(__dirname, 'dist'),
-		compress: true,
-		port: 9000,
-		open: true
-	}
+function getEntries() {
+  try {
+    const entries = {};
+    const allEntry = globby.sync('src/pages/**/main.js');
+    allEntry.forEach(entry => {
+      const res = entry.match(/src\/pages\/(\w+)\/main\.js/);
+      if (res.length) {
+        entries[res[1]] = `./${entry}`;
+      }
+    });
+    return entries;
+  } catch (error) {
+    console.error('File structure is incorrect for MPA');
+  }
+}
+
+function multiHtmlPlugin(entries) {
+  const pageNames = Object.keys(entries);
+  return pageNames.map(name => {
+    return new HtmlWebpackPlugin({
+      filename: `${name}.html`,
+      template: './public/index.html',
+      chunks: [name],
+    });
+  });
+}
+
+module.exports = function() {
+  const entry = getEntries();
+
+  const htmlPlugins = multiHtmlPlugin(entry);
+
+  const config = {
+    entry,
+    resolve: {
+      extensions: ['.mjs', '.js', '.svelte'],
+    },
+    output: {
+      path: path.join(__dirname, 'dist'),
+      filename: '[name].js',
+      chunkFilename: '[name].[id].js',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.svelte$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'svelte-loader',
+            options: {
+              emitCss: true,
+              hotReload: true,
+            },
+          },
+        },
+        {
+          test: /\.css$/,
+          use: [
+            /**
+             * MiniCssExtractPlugin doesn't support HMR.
+             * For developing, use 'style-loader' instead.
+             * */
+            prod ? MiniCssExtractPlugin.loader : 'style-loader',
+            'css-loader',
+          ],
+        },
+      ],
+    },
+    mode,
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: '[name].css',
+      }),
+      new CopyPlugin([{ from: './public', to: './public' }]),
+      ...htmlPlugins,
+    ],
+    devtool: prod ? false : 'source-map',
+    devServer: {
+      contentBase: path.join(__dirname, 'dist'),
+      compress: true,
+      port: 9000,
+      open: true,
+    },
+  };
+
+  return config;
 };
