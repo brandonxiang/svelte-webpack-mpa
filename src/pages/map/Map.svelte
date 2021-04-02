57afe7cd0f92fd87c75c174ca6ba1a2acb9205a3
Weiping Xiang <weiping.xiang@shopee.com>
2021-04-03 00:42:59 +0800

    chore: update webpack 5

--- src/pages/map/Map.svelte
+++ src/pages/map/Map.svelte
@@ -20,17 +20,9 @@
     script.onload = () => {
       map = L.map(container).setView([lat, lon], zoom);
 
-      L.tileLayer(
-        'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}',
-        {
-          attribution:
-            'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-          maxZoom: 18,
-          id: 'mapbox.streets',
-          accessToken:
-            'pk.eyJ1IjoieHdwaXNtZSIsImEiOiJ5cTlCQTlRIn0.QdV-wNUKbgs7jAlbVE747Q',
-        }
-      ).addTo(map);
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      }).addTo(map);
     };
 
     document.head.appendChild(script);
